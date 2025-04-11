f1a0d2519d3109f2c868d2502d5f27ca92121aab
chrissnts <nicokotzias@gmail.com>
2025-04-11 11:44:45 -0300

    classes finshed

---
+++ Condutor.java
@@ -0,0 +1,26 @@
+public class Condutor {
+    private String nome;
+    private int cnh;
+
+    public Condutor(String nome, int cnh){
+        this.nome = nome;
+        this.cnh = cnh;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(int cnh) {
+        this.cnh = cnh;
+    }
+}
+
